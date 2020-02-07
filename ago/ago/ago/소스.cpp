34c08f3f62e1bb59cf8b73f78de89c527c4b7e52
leleluv1122 <jh_o214@naver.com>
2020-02-07 19:43:30 +0900

    leleluv1122

---
+++ ago/ago/ago/소스.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	int row = 5, col = 10;
+	int** arr = NULL;
+	printf("2차원 행의 개수: ");
+	scanf_s("%d", &row);
+	printf("2차원 열의 개수: ");
+	scanf_s("%d", &col);
+
+	arr = (int**)malloc(sizeof(int*) * row);
+	for (int i = 0; i < row; ++i)
+		arr[i] = (int*)malloc(sizeof(int) * col);
+
+	for (int i = 0; i < row; ++i)
+		for (int j = 0; j < col; ++j)
+			arr[i][j] = (i * col + (j + 1)) * 10;
+
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < col; ++j)
+			printf("%3d ", arr[i][j]);
+		printf("\n");
+	}
+	for (int i = 0; i < row; ++i)
+		free(arr[i]);
+}
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main() {
+//	int arr[3][3] = { 0 };
+//
+//	for (int i = 0; i < 3; ++i) {
+//		for (int j = 0; j < 3; ++j) {
+//			arr[i][j] = i * 3 + (j + 1);
+//		}
+//	}
+//	for (int i = 0; i < 3; ++i) {
+//		for (int j = 0; j < 3; ++j)
+//			printf("%d ", arr[i][j]);
+//		printf("\n");
+//	}
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main() {
+//	int arr[5] = { 0 };
+//	arr[0] = 11;
+//
+//	for (int i = 1; i < 5; ++i)
+//		arr[i] = 11 + arr[i - 1];
+//
+//	for (int i = 0; i < 5; ++i)
+//		printf("arr[%d] : %d\n", i, arr[i]);
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main() {
+//	int* arr = (int*)malloc(sizeof(int) * 5);
+//
+//	for (int i = 0; i < 5; ++i)
+//		arr[i] = 11 * (i + 1);
+//
+//	for (int i = 0; i < 5; ++i)
+//		printf("arr[%d]: %d\n", i, arr[i]);
+//
+//	free(arr);
+//}
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int total;
+//	printf("메모리의 개수를 입력: ");
+//	scanf_s("%d", &total);
+//
+//	int* arr = (int*)malloc(total * sizeof(int));
+//
+//	for (int i = 0; i < total; ++i)
+//		arr[i] = i + 1;
+//	for (int i = 0; i < total; ++i)
+//		printf("%d ", arr[i]);
+//	printf("\n");
+//
+//	free(arr);
+//}
